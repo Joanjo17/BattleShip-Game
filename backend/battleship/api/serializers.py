3330e6e7ecbc00729064a26f33220d2979fc29b3
AimanDoul04 <aiman2004doul@gmail.com>
2025-05-04 21:21:03 +0000

    feat(api): agregar soporte para Board, Vessel, BoardVessel y Shot.
    -models: añadidos modelos Vessel, Board, BoardVessel y Shot con FK y related_name apropiados.
    - serializers: creados ModelSerializers mínimos para los nuevos modelos.
    - views: implementados ViewSets y registrados en el router.
    - migrations: generada y aplicada la migración de estas tablas.

--- backend/battleship/api/serializers.py
+++ backend/battleship/api/serializers.py
@@ -1,8 +1,48 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Player, Game, Board, Vessel, BoardVessel, Shot
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
         # exclude = ('password',)
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+
+class GameSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.user.username')
+
+    class Meta:
+        model = Game
+        fields = '__all__'
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = '__all__'
+
+
+class VesselSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vessel
+        fields = '__all__'
+
+
+class BoardVesselSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BoardVessel
+        fields = '__all__'
+
+
+class ShotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shot
+        fields = '__all__'
--- backend/battleship/api/views.py
+++ backend/battleship/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, status
 from django.contrib.auth.models import User
-from . import serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
+from . import models
+from .models import Game, Player, Board, BoardVessel, Shot, Vessel
+from . import serializers
+from .serializers import PlayerSerializer, GameSerializer, BoardSerializer, BoardVesselSerializer, ShotSerializer, VesselSerializer
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -11,3 +17,41 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nickname']
+
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['phase']
+    ordering_fields = ['id']
+
+    def perform_create(self, serializer):
+        player = get_object_or_404(models.Player, user=User.objects.first())
+        serializer.save(owner=player)
+
+
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+
+
+class BoardVesselViewSet(viewsets.ModelViewSet):
+    queryset = BoardVessel.objects.all()
+    serializer_class = BoardVesselSerializer
+
+
+class VesselViewSet(viewsets.ModelViewSet):
+    queryset = Vessel.objects.all()
+    serializer_class = VesselSerializer
+
+
+class ShotViewSet(viewsets.ModelViewSet):
+    queryset = Shot.objects.all()
+    serializer_class = ShotSerializer
